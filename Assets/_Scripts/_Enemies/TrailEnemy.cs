a85b5392d4b9eb70f66e7f2d4674c05557c484ae
mapsdos <arnauprohens@gmail.com>
2026-05-21 19:20:52 +0200

    Models aplicats, i aranya spawneja webs on s'havia mogut, i no a on se mou

--- Assets/_Scripts/_Enemies/TrailEnemy.cs
+++ Assets/_Scripts/_Enemies/TrailEnemy.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class TrailEnemy : EnemyFollower {
     public GameObject slimePrefab;
-    protected override void FinishMovement() {
-        // Drop slime at previous position before calling base
+
+    protected override void StartMovement() {
+        // Spawn cowweb at current position before moving
         Vector3 spawnPos = new Vector3(Mathf.Round(transform.position.x), transform.position.y, Mathf.Round(transform.position.z));
-        Instantiate(slimePrefab, spawnPos - Vector3.up*0.45f, Quaternion.identity);
-        base.FinishMovement();
+        Instantiate(slimePrefab, spawnPos - Vector3.up * 0.45f, Quaternion.identity);
+        base.StartMovement();
     }
 }
